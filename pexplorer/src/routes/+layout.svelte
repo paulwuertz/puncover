a661dc92d1ee3f5f4a5b03c45dff4566598ff7ff
paulwuertz <paulwuertz@posteo.de>
2025-04-06 21:10:33 +0200

    add nav header with many todos :)

---
+++ pexplorer/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    let { children } = $props();
+    import { Navbar, NavItem, NavLink, NavbarBrand } from '@sveltestrap/sveltestrap';
+</script>
+
+<nav>
+  <Navbar color="light">
+    <NavbarBrand href="/">Puncover</NavbarBrand>
+
+    <NavItem><NavLink href="/symbols">Symbols</NavLink></NavItem>
+    <NavItem><NavLink href="/threads">Thread (TODO)</NavLink></NavItem>
+    <NavItem><NavLink href="/interrupts">Interrupts (TODO)</NavLink></NavItem>
+    <NavItem><NavLink href="/settings">Settings (TODO)</NavLink></NavItem>
+    <NavItem><NavLink href="/about">About</NavLink></NavItem>
+  </Navbar>
+  <!--- TODO add breadcrumbs-->
+</nav>
+
+{@render children()}
