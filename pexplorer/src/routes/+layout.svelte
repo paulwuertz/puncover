a1065f20f3435694d8262c3b3aeae906d3adc2f8
paulwuertz <paulwuertz@posteo.de>
2025-04-13 16:24:31 +0200

    try to setup static export for gh pages

--- pexplorer/src/routes/+layout.svelte
+++ pexplorer/src/routes/+layout.svelte
@@ -1,17 +1,18 @@
 <script>
     let { children } = $props();
+    import { base } from '$app/paths';
     import { Navbar, NavItem, NavLink, NavbarBrand } from '@sveltestrap/sveltestrap';
 </script>
 
 <nav>
   <Navbar color="light">
-    <NavbarBrand href="/">Puncover</NavbarBrand>
+    <NavbarBrand href="{base}/">Puncover</NavbarBrand>
 
-    <NavItem><NavLink href="/symbols">Symbols</NavLink></NavItem>
-    <NavItem><NavLink href="/threads">Thread (TODO)</NavLink></NavItem>
-    <NavItem><NavLink href="/interrupts">Interrupts (TODO)</NavLink></NavItem>
-    <NavItem><NavLink href="/settings">Settings (TODO)</NavLink></NavItem>
-    <NavItem><NavLink href="/about">About</NavLink></NavItem>
+    <NavItem><NavLink href="{base}/symbols">Symbols</NavLink></NavItem>
+    <NavItem><NavLink href="{base}/threads">Thread (TODO)</NavLink></NavItem>
+    <NavItem><NavLink href="{base}/interrupts">Interrupts (TODO)</NavLink></NavItem>
+    <NavItem><NavLink href="{base}/settings">Settings (TODO)</NavLink></NavItem>
+    <NavItem><NavLink href="{base}/about">About</NavLink></NavItem>
   </Navbar>
   <!--- TODO add breadcrumbs-->
 </nav>
