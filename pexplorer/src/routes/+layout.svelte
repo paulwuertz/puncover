241030fceaa4f7ffc77875f1f82a1fbbd76e4530
paulwuertz <paulwuertz@posteo.de>
2025-04-18 02:54:22 +0200

    mobile friendly nav

--- pexplorer/src/routes/+layout.svelte
+++ pexplorer/src/routes/+layout.svelte
@@ -1,18 +1,29 @@
 <script>
     let { children } = $props();
     import { base } from '$app/paths';
-    import { Navbar, NavItem, NavLink, NavbarBrand } from '@sveltestrap/sveltestrap';
+    import { Collapse, Nav, Navbar, NavbarBrand, NavItem, NavLink, NavbarToggler } from '@sveltestrap/sveltestrap';
+
+    let isOpen = $state(true);
+
+    function handleUpdate(event) {
+        isOpen = event.detail.isOpen;
+    }
 </script>
 
 <nav>
-  <Navbar color="light">
+  <Navbar color="light" light expand="md" container="md">
     <NavbarBrand href="{base}/">Puncover</NavbarBrand>
-
-    <NavItem><NavLink href="{base}/symbols">Symbols</NavLink></NavItem>
-    <NavItem><NavLink href="{base}/threads">Thread (TODO)</NavLink></NavItem>
-    <NavItem><NavLink href="{base}/interrupts">Interrupts (TODO)</NavLink></NavItem>
-    <NavItem><NavLink href="{base}/settings">Settings (TODO)</NavLink></NavItem>
-    <NavItem><NavLink href="{base}/about">About</NavLink></NavItem>
+    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav class="ms-auto" navbar>
+            <NavItem><NavLink href="{base}/symbols">Symbols</NavLink></NavItem>
+            <NavItem><NavLink href="{base}/threads">Thread (TODO)</NavLink></NavItem>
+            <NavItem><NavLink href="{base}/interrupts">Interrupts (TODO)</NavLink></NavItem>
+            <NavItem><NavLink href="{base}/diff">Compare Versions</NavLink></NavItem>
+            <NavItem><NavLink href="{base}/settings">Settings (TODO)</NavLink></NavItem>
+            <NavItem><NavLink href="{base}/about">About</NavLink></NavItem>
+        </Nav>
+    </Collapse>
   </Navbar>
   <!--- TODO add breadcrumbs-->
 </nav>
