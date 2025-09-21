97883c0169668ae879ac95ed33900f8c3ec5a71a
paulwuertz <paulwuertz@posteo.de>
2025-09-21 21:39:20 +0200

    remove pexplorer - moved to it's own repo

--- pexplorer/src/routes/diff/+layout.js
+++
@@ -1,4 +0,0 @@
-export const trailingSlash = 'always';
-export const prerender = true;
-export const ssr = false;
-export const csr = true;
--- pexplorer/src/routes/symbols.svelte.js
+++
@@ -1,6 +0,0 @@
-export const symbols = $state({
-    symbols: {},
-    selected_version: null,
-    selected_versions_to_compare: null,
-    elfDataProvided: false
-});
